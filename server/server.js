404b24b0dc165902c97dc40d91bafc713d250ff4
Patti Perlock <pattiperlock@gmail.com>
2021-02-10 10:08:23 -0500

    Testing index.html

--- server/server.js
+++ server/server.js
@@ -33,8 +33,7 @@ mongoose.connect(process.env.MONGO_DBURI, {useNewUrlParser: true, useUnifiedTopo
 
 if (process.env.NODE_ENV === "production") {
     // Set static folder
-    // app.use(express.static("../client/build"));
-    app.use('/static', express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static("../client/build"));
   
     app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
