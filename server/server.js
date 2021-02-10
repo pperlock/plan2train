ee2804b32792d73d2a5d0ef6357b97666a78ab04
Patti Perlock <pattiperlock@gmail.com>
2021-02-10 17:13:54 -0500

    Changed environment get to / instead of *

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === "production") {
     // Set static folder
     app.use(express.static("../client/build"));
   
-    app.get("*", (req, res) => {
+    app.get("/", (req, res) => {
       res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
     });
   }
